5fbb80434053e7fa4f79f44454d2d826b5311dd6
HP <radadiyakrish62@gmail.com>
2024-06-21 10:37:36 +0530

    cloths app

---
+++ android/app/src/main/kotlin/com/example/cloths_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cloths_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+    getPages: [],
+  ));
+}
